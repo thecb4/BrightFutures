6580d864d5b2e8809d5112dde3c3d83dd8946c74
TheCB4 <cavelle@thecb4.io>
2018-04-01 15:54:53 -0400

    using thecb4 dependencies

--- Package.swift
+++ Package.swift
@@ -2,7 +2,28 @@ import PackageDescription
 
 let package = Package(
     name: "BrightFutures",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+          name: "BrightFutures",
+          targets: ["BrightFutures"])
+    ],
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "https://github.com/thecb4/Result.git", .upToNextMinor(from: "0.6.0"))
+//      .package(url: "https://github.com/freshOS/then.git",  .upToNextMinor(from: "3.0.2"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "BrightFutures",
+            dependencies: ["Result"],
+            path: "Sources",
+            sources:["BrightFutures/Core"]),
+        .testTarget(
+            name: "BrightFuturesUnitTests",
+            dependencies: ["Result","BrightFutures"]),
     ]
 )
